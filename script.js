142466aab9c696450f603515c0e1b38a532ae96f
juanmarab <juanmanueluruguay21@gmail.com>
2024-09-30 18:29:52 -0300

    wip about me and skills chart

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-// Gráfico de habilidades técnicas
-var ctx = document.getElementById('skillsChart').getContext('2d');
-var skillsChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        labels: ['JavaScript', 'Angular', 'C#', '.NET Core', 'SQL'],
-        datasets: [{
-            data: [25, 30, 20, 15, 10],
-            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF']
-        }]
-    }
-});
+// Asegúrate de que el gráfico se carga cuando la página esté completamente cargada
+window.onload = function() {
+    var ctx = document.getElementById('skillsChart').getContext('2d');
+    var skillsChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ['JavaScript', 'Angular', 'C#', '.NET Core', 'SQL'],
+            datasets: [{
+                data: [25, 30, 20, 15, 10],
+                backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF']
+            }]
+        }
+    });
+};
