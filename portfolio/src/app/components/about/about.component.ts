ccca4bda6c07e3dd37df41c972d499f9b0442f24
juanmarab <juanmanueluruguay21@gmail.com>
2025-01-15 22:29:21 -0300

    wip

--- portfolio/src/app/components/about/about.component.ts
+++ portfolio/src/app/components/about/about.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
-  title: string = "About Me";
-  description: string = "I am a Full-Stack developer based in Montevideo, Uruguay. Currently, I am an advanced student pursuing a Bachelor's degree in Systems at ORT University. I have developed a strong passion for the field of systems and am motivated by the positive impact that technology can have on society.";
+  
 }
