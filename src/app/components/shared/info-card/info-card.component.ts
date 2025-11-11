4f47c2a3a767136d8b7787c8b248023c97bf9b6f
juanmarab <juanmanueluruguay21@gmail.com>
2025-11-11 13:57:23 -0300

    new component generic info card

---
+++ src/app/components/shared/info-card/info-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-info-card',
+  templateUrl: './info-card.component.html',
+  styleUrls: ['./info-card.component.css']
+})
+export class InfoCardComponent {
+
+  @Input() logo?: string; // URL de la imagen (opcional)
+  @Input() title: string = ''; // Título (ej. "Kinamic Technologies")
+  @Input() subtitle: string = ''; // Subtítulo (ej. "Full Stack Developer")
+  @Input() dates: string = ''; // Rango de fechas
+  @Input() points: string[] = []; // Array de strings para los bullet points
+
+  constructor() { }
+}
